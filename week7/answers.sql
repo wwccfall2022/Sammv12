b430475dae331bb135d0f89286f607f96a0ee9c2
Sammv12 <113943852+Sammv12@users.noreply.github.com>
2022-11-03 19:00:01 -0700

    Added players table

--- week7/answers.sql
+++ week7/answers.sql
@@ -1,3 +1,10 @@
 -- Create your tables, views, functions and procedures here!
 CREATE SCHEMA destruction;
 USE destruction;
+
+CREATE TABLE players (
+	player_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
