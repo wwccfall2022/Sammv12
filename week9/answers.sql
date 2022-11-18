d8ab4905f364d05b83c1a379ae3202f270c606df
Sammv12 <113943852+Sammv12@users.noreply.github.com>
2022-11-18 10:04:09 -0800

    Added session

--- week9/answers.sql
+++ week9/answers.sql
@@ -11,3 +11,15 @@ CREATE TABLE users (
 );
 
 
+CREATE TABLE sessions (
+	session_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id INT UNSIGNED NOT NULL,
+	created_on TIMESTAMP NOT NULL DEFAULT NOW(),
+	updated_on TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW(),
+	
+	CONSTRAINT sessions_fk_users
+		FOREIGN KEY (user_id)
+		REFERENCES users (user_id)
+        	ON UPDATE CASCADE
+        	ON DELETE CASCADE
+);
