541eec580bc99e89885fbc48aae8d70359cf39f5
Sammv12 <113943852+Sammv12@users.noreply.github.com>
2022-11-18 10:06:27 -0800

    added friends

--- week9/answers.sql
+++ week9/answers.sql
@@ -23,3 +23,23 @@ CREATE TABLE sessions (
         	ON UPDATE CASCADE
         	ON DELETE CASCADE
 );
+
+
+
+CREATE TABLE friends (
+	user_friend_id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	user_id INT UNSIGNED NOT NULL,
+	friend_id INT UNSIGNED NOT NULL,
+	
+	CONSTRAINT friends_fk_users
+		FOREIGN KEY (user_id)
+		REFERENCES users(user_id)
+        	ON UPDATE CASCADE
+        	ON DELETE CASCADE,
+        
+	CONSTRAINT friends_fk_users
+		FOREIGN KEY (friend_id)
+		REFERENCES users(user_id)
+        	ON UPDATE CASCADE
+        	ON DELETE CASCADE
+);
