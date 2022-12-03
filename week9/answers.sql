263b2e8bb383b769dab9f2560a45e4f5e4b5d120
Sammv12 <113943852+Sammv12@users.noreply.github.com>
2022-12-02 20:10:12 -0800

    fix procedure

--- week9/answers.sql
+++ week9/answers.sql
@@ -169,7 +169,7 @@ DECLARE row_not_found TINYINT DEFAULT FALSE;
 DECLARE friend_cursor CURSOR FOR 
 
 SELECT friend_id FROM friends
-WHERE user_id = id_user
+WHERE user_id = user_id
 GROUP BY friend_id;
 
 
